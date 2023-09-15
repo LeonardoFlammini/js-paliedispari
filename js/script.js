79aed9121c288c1208b4b669eb8f5c256ff15373
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-15 16:29:02 +0200

    [add]script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const word = prompt("Inserire una parola");
 
+console.log(isPal(word));
 
 function isPal(word){
-  
+  const wordInToArray = word.split("");
+  const reverseWord = [];
+
+  for ( let i = 0; i< wordInToArray.length; i++){
+    
+  }
+
+  console.log(wordInToArray);
+  return true;
 }
