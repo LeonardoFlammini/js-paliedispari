b8dec87b4a8b0e6a937e6c2026431936ac6c022d
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-15 16:43:21 +0200

    [add] palindroma fatto

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 const word = prompt("Inserire una parola");
+const palOut = document.getElementById("pal-out");
 
-console.log(isPal(word));
+if(isPal(word)){
+  palOut.innerHTML = "La parola " + word + " è palindroma"
+}else {
+  palOut.innerHTML = "La parola " + word + " NON è palindroma"
+}
+
 
 function isPal(word){
   const wordInToArray = word.split("");
-  const reverseWord = [];
+  const reverseWordArray = [];
+  const conditionIndex = wordInToArray.length - 1;
 
-  for ( let i = 0; i< wordInToArray.length; i++){
-    
+  for ( let i = 0; i <= conditionIndex; i++){
+    reverseWordArray[i] = wordInToArray[conditionIndex - i];
   }
+  console.log(reverseWordArray);
 
-  console.log(wordInToArray);
-  return true;
+  const wordRev = reverseWordArray.join("");
+  console.log(wordRev + " Parola al contrario");
+
+  if ( word === wordRev){
+    return true;
+  }else{
+    return false;
+  }
 }
